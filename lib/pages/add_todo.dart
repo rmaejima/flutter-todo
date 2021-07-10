3b58633b7ce37f41ebed7b56fdb0081544a122ae
rmaejima <ryo.maejii@gmail.com>
2021-07-10 16:00:54 +0900

    Add TodoList page layout

--- lib/pages/add_todo.dart
+++ lib/pages/add_todo.dart
@@ -4,12 +4,44 @@ class AddTodoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text("リスト追加画面（クリックで戻る）"),
+      appBar: AppBar(
+        title: Text("リスト追加"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //入力フィールド
+            TextField(),
+            //padding代わりに使ってる
+            const SizedBox(
+              height: 8,
+            ),
+            Container(
+              //横いっぱいに広げる
+              width: double.infinity,
+              child: ElevatedButton(
+                // color: Colors.blue,
+                onPressed: () {},
+                child: Text('リスト追加', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // キャンセルボタン
+              child: TextButton(
+                // ボタンをクリックした時の処理
+                onPressed: () {
+                  // "pop"で前の画面に戻る
+                  Navigator.of(context).pop();
+                },
+                child: Text('キャンセル'),
+              ),
+            ),
+          ],
         ),
       ),
     );
